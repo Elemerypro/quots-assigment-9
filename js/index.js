484a4eda96fb7db834cb5c402c06950e783b0d95
manajs95 <eng.elemery@gmail.com>
2024-05-30 18:13:52 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,210 @@
+
+
+var quots = [
+    {
+        quot: "“Be yourself; everyone else is already taken.”",
+        author: "Oscar Wilde"
+    },
+    {
+        quot: "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
+        author: "― Albert Einstein"
+    },
+    {
+        quot: "“You only live once, but if you do it right, once is enough.”",
+        author: "― Mae West"
+    },
+    {
+        quot: "“If you tell the truth, you don't have to remember anything.”",
+        author: "― Mark Twain"
+    },
+    {
+        quot: "“Always forgive your enemies; nothing annoys them so much.”",
+        author: "Oscar Wilde"
+    },
+    {
+        quot: "“Live as if you were to die tomorrow. Learn as if you were to live forever.”",
+        author: "― Mahatma Gandhi"
+    },
+    {
+        quot: "“Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.”",
+        author: "― H. Jackson Brown Jr., P.S. I Love You"
+    },
+    {
+        quot: "“The person, be it gentleman or lady, who has not pleasure in a good novel, must be intolerably stupid.”",
+        author: "― Jane Austen, Northanger Abbey"
+    },
+    {
+        quot: "“It is our choices, Harry, that show what we truly are, far more than our abilities.”",
+        author: "― J.K. Rowling, Harry Potter and the Chamber of Secrets"
+    },
+    {
+        quot: "“There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.”",
+        author: "― Albert Einstein"
+    },
+    {
+        quot: "“Good friends, good books, and a sleepy conscience: this is the ideal life.”",
+        author: "― Mark Twain"
+    },
+    {
+        quot: "“The fool doth think he is wise, but the wise man knows himself to be a fool.”",
+        author: "― William Shakespeare, As You Like It"
+    },
+    {
+        quot: "“Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.”",
+        author: "― Bill Keane"
+    },
+    {
+        quot: "“I have not failed. I've just found 10,000 ways that won't work.”",
+        author: "― Thomas A. Edison"
+    },
+    {
+        quot: "“It is not a lack of love, but a lack of friendship that makes unhappy marriages.”",
+        author: "― Friedrich Nietzsche"
+    },
+    {
+        quot: "“I may not have gone where I intended to go, but I think I have ended up where I needed to be.”",
+        author: "― douglas adams, The Long Dark Tea-Time of the Soul"
+    },
+    {
+        quot: "“Sometimes, you read a book and it fills you with this weird evangelical zeal, and you become convinced that the shattered world will never be put back together unless and until all living humans read the book.”",
+        author: "― John Green, The Fault in Our Stars"
+    },
+    {
+        quot: "“Life isn't about finding yourself. Life is about creating yourself.”",
+        author: "― George Bernard Shaw"
+    },
+    {
+        quot: "“Beauty is in the eye of the beholder and it may be necessary from time to time to give a stupid or misinformed beholder a black eye.”",
+        author: "― Jim Henson"
+    },
+    {
+        quot: "“Of course it is happening inside your head, Harry, but why on earth should that mean that it is not real?”",
+        author: "― J.K. Rowling, Harry Potter and the Deathly Hallows"
+    },
+    {
+        quot: "“If one cannot enjoy reading a book over and over again, there is no use in reading it at all.”",
+        author: "Oscar Wilde"
+    },
+    {
+        quot: "“Success is not final, failure is not fatal: it is the courage to continue that counts.”",
+        author: "― Winston S. Churchill"
+    },
+    {
+        quot: "“One good thing about music, when it hits you, you feel no pain.”",
+        author: "― Bob Marley"
+    },
+    {
+        quot: "““I find television very educating. Every time somebody turns on the set, I go into the other room and read a book.”",
+        author: "― Groucho Marx"
+    },
+    {
+        quot: "“Love is like the wind, you can't see it but you can feel it.”",
+        author: "― Nicholas Sparks, A Walk to Remember"
+    },
+    {
+        quot: "“When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.”",
+        author: "― Helen Keller"
+    },
+    {
+        quot: "“There is no pretending,' Jace said with absolute clarity. 'I love you, and I will love you until I die, and if there is life after that, I'll love you then.”",
+        author: "― Cassandra Clare, City of Glass"
+    },
+    {
+        quot: "“The real lover is the man who can thrill you by kissing your forehead or smiling into your eyes or just staring into space.”",
+        author: "― Marilyn Monroe"
+    },
+    {
+        quot: "“The only way out of the labyrinth of suffering is to forgive.”",
+        author: "― John Green, Looking for Alaska"
+    },
+    {
+        quot: "“When someone loves you, the way they talk about you is different. You feel safe and comfortable.”",
+        author: "― Jess C. Scott, The Intern"
+    },
+    {
+        quot: "“And, when you want something, all the universe conspires in helping you to achieve it.”",
+        author: "― Paulo Coelho, The Alchemist"
+    },
+    {
+        quot: "“The reason I talk to myself is because I’m the only one whose answers I accept.”",
+        author: "― George Carlin"
+    },
+    {
+        quot: "“He's like a drug for you, Bella.”",
+        author: "― Stephenie Meyer, Eclipse"
+    },
+    {
+        quot: "“Do not pity the dead, Harry. Pity the living, and, above all those who live without love.”",
+        author: "― J.K. Rowling, Harry Potter and the Deathly Hallows"
+    },
+    {
+        quot: "“There is nothing to writing. All you do is sit down at a typewriter and bleed.”",
+        author: "― Ernest Hemingway"
+    },
+    {
+        quot: "“A wise girl kisses but doesn't love, listens but doesn't believe, and leaves before she is left.”",
+        author: "― Marilyn Monroe"
+    },
+    {
+        quot: "“You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.”",
+        author: "― John Lennon"
+    },
+    {
+        quot: "“You don’t forget the face of the person who was your last hope.”",
+        author: "― Suzanne Collins, The Hunger Games"
+    },
+    {
+        quot: "“I am good, but not an angel. I do sin, but I am not the devil. I am just a small girl in a big world trying to find someone to love.”",
+        author: "― Marilyn Monroe"
+    },
+    {
+        quot: "“I am free of all prejudice. I hate everyone equally. ”",
+        author: "― W.C. Fields"
+    },
+    {
+        quot: "“You never really understand a person until you consider things from his point of view... Until you climb inside of his skin and walk around in it.”",
+        author: "― Harper Lee, To Kill a Mockingbird"
+    },
+    {
+        quot: "“I have never let my schooling interfere with my education.”",
+        author: "― Mark Twain"
+    },
+    {
+        quot: "“Give a man a fire and he's warm for a day, but set fire to him and he's warm for the rest of his life.”",
+        author: "― Terry Pratchett, Jingo"
+    },
+    {
+        quot: "“Never forget what you are, for surely the world will not. Make it your strength. Then it can never be your weakness. Armour yourself in it, and it will never be used to hurt you.”",
+        author: "― George R.R. Martin, A Game of Thrones"
+    },
+    {
+        quot: "“Happiness is when what you think, what you say, and what you do are in harmony.”",
+        author: "― Mahatma Gandhi"
+    },
+    {
+        quot: "“Keep away from people who try to belittle your ambitions. Small people always do that, but the really great make you feel that you, too, can become great.”",
+        author: "― Mark Twain"
+    },
+
+]
+
+
+
+
+function addQuote() {
+    
+    var randomIndex = Math.floor(Math.random() * quots.length);
+
+    
+    var randomQuote = quots[randomIndex].quot;
+    var randomAuthor = quots[randomIndex].author;
+
+    
+    document.getElementById("quoteOutput").innerHTML = randomQuote;
+    document.getElementById("authorOutput").innerHTML = randomAuthor;
+}
+
+addQuote();
+
+
+
